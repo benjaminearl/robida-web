93178ade92d4c595f32d1df59ed6fbd77aa3d319
benjaminearl <bnjmnearl@gmail.com>
2022-03-31 17:22:42 +0200

    add quick menu to navigate

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,2 +1,21 @@
 <h1><?= $page->title() ?></h1>
 <p><?= $page->Text() ?></p>
+
+
+<?php
+
+// main menu items
+$items = $pages->listed();
+
+// only show the menu if items are available
+if($items->isNotEmpty()):
+
+?>
+<nav>
+  <ul>
+    <?php foreach($items as $item): ?>
+    <li><a<?php e($item->isOpen(), ' class="active"') ?> href="<?= $item->url() ?>"><?= $item->title()->html() ?></a></li>
+    <?php endforeach ?>
+  </ul>
+</nav>
+<?php endif ?>
