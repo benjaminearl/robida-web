992ff806547f65732420d38518ca21eb502183e9
benjaminearl <bnjmnearl@gmail.com>
2022-05-31 10:23:31 +0200

    update block test

--- site/templates/entry.php
+++ site/templates/entry.php
@@ -7,7 +7,7 @@
 <small>re: <?php $relatedpages =  $page->related()->toPages();  foreach($relatedpages as $relatedpage): ?><a href="<?= $relatedpage->url() ?>"><?= $relatedpage->title() ?></a></small><br>
 <?php endforeach ?><br>
 <?= $page->image() ?><br>
-<?= $page->myBlocksField()->toBlocks() ?>
+<?= $page->text()->toBlocks() ?>
 <small><?php foreach ($page->tags()->split() as $tag): ?>
   <a href="<?= url('journal', ['params' => ['tag' => $tag]]) ?>">#<?= html($tag) ?></a>
 <?php endforeach ?></small>
