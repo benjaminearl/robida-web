9e440b6b814b39c7b83c36596e7aa50cb41d847e
benjaminearl <bnjmnearl@gmail.com>
2022-06-09 13:03:23 +0200

    delete keys

--- site/config/config.php
+++ site/config/config.php
@@ -7,7 +7,3 @@
      ]
  ];
 
- return [
-    'benediktengel.google-calendar-plugin.apikey' => 'AIzaSyB3Tg8w1E0LPx2o5Zmglp8g52WBxzX8760',
-    'benediktengel.google-calendar-plugin.calendarID' => 'meo3754sq65v3etl70m096pbjc@group.calendar.google.com'
- ];
