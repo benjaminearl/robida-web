dc2d2024a7e7b2be6a1fb975d4676f4c1fd2fa1a
benjaminearl <bnjmnearl@gmail.com>
2022-07-25 16:47:19 +0200

    make parent links unclickable in nav

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -16,7 +16,7 @@ $items = $pages->listed();
       <ul>
         <?php foreach($items as $item): ?>
           <?php if ($item->hasChildren()): ?>
-            <li class="container"><p><a<?php e($item->isOpen(), ' class="active"') ?> href="<?= $item->url() ?>"><?= $item->title()->html() ?></a></p>
+            <li class="container"><p><a<?php e($item->isOpen(), ' class="active"') ?> href="<?= $item->url() ?>" style="pointer-events: none;"><?= $item->title()->html() ?></a></p>
               <ul>
                 <?php $children =  $item->children();  foreach($children as $child): ?>
                   <li><p><a<?php e($child->isOpen(), ' class="active"') ?> href="<?= $child->url() ?>"><?= $child->title()->html() ?></a></p></li>
