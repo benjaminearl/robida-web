9d09a3a1ae881b981b2888adc930ca49c61864f5
k-spruit <kirstenspruit@gmail.com>
2022-08-09 14:25:02 +0200

    fontsize

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -27,7 +27,7 @@
       <a href="<?= $site->url() ?>"><img id="logo" src="https://robidacollective.com/assets/images/robida_logo_vector.png" alt="Robida"></a>
     </div>
     <div class="audio green-audio-player">
-    <p style="display:inline-block; padding-right: 20px;">Radio Robida</p>
+    <p style="display:inline-block; padding-right: 1em;">Radio Robida</p>
       <div class="play-pause-btn">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 18 24">
           <path fill="#000000" fill-rule="evenodd" d="M18 12L0 24V0" class="play-pause-icon" id="playPause"/>
