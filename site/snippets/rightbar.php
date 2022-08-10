9d86017339e0a333b1b1f717d08d77b9be5cbe57
benjaminearl <bnjmnearl@gmail.com>
2022-08-10 10:35:56 +0200

    style glossary spacing

--- site/snippets/rightbar.php
+++ site/snippets/rightbar.php
@@ -1,5 +1,5 @@
 <div id="rightSidebar">
-  <span id="closeRight" onclick="closeGlossary()">Glossary</span>
+  <span id="closeRight" onclick="closeGlossary()">Glossary [X]</span>
   <span id="openRight" onclick="openGlossary()">A.B.C</span>
   <div id="glossary-content">
     <?= kt(page('glossary')->text()) ?>
